d0c950d5420abf9a635ba40a8b2ccec58dae9643
Originalhans <hansrichmond9@gmail.com>
2023-12-14 16:56:43 -0800

    Hello , printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
  * File: 5-printf.c
- * Auth: Hans Guckel
+ * Auth: Hans Guckel Richmond
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a
- *                 multilingual puzzle," followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 } 
